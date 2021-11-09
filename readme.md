5f8f97348223a6844bf75fb8115194b3abfaee57
Julian van Meggelen <julianvanmeggelen@gmail.com>
2021-11-09 19:35:41 +0100

    updated readme

---
+++ readme.md
@@ -0,0 +1,11 @@
+# npzigzag
+This is a simple numpy implementation of a zigzag indicator.
+
+## Usage
+```python
+pip install npzigzag
+```
+```python
+from npzigzag import core as zz
+```
+
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='npzigzag',
     packages=find_packages(include=['npzigzag']),
-    version='0.1.0',
+    version='0.1',
     description='Vanilla numpy implementation of zigzag indicator.',
     author='Julian van Meggelen',
     license='MIT',
@@ -13,5 +13,19 @@ setup(
         ],
     setup_requires=['pytest-runner'],
     tests_require=['pytest==6.2.5'],
-    test_suite='npzigzag_tests'
+    test_suite='npzigzag_tests',
+    author_email = 'julianvanmeggelen@icloud.com',      
+    url = 'https://github.com/julianvanmeggelen/npzigzag',  
+    download_url = 'https://github.com/julianvanmeggelen/npzigzag/archive/refs/tags/v01.tar.gz',    # I explain this later on
+    keywords = ['zigzag', 'zigzag indicator'],   
+    classifiers=[
+        'Development Status :: 3 - Alpha',      #  "3 - Alpha", "4 - Beta" or "5 - Production/Stable" 
+        'Intended Audience :: Developers',      # Define that your audience are developers
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',   #license
+        'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ]
 )
